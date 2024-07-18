4d721b8021c0d7573ada149a02f9a6d6044485c2
Hirunithathsara <hitunithathsaralokuthanthri@gmail.com>
2024-07-18 10:09:51 +0530

    Refactored

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { View,Text } from 'react-native'
+import { View,Text,Image, TouchableOpacity } from 'react-native'
 
 const index = () => {
   return (
-   <View>
-    <Text ><h1>Instagram</h1></Text>
+   <View style={{paddingHorizontal:15,
+                 flexDirection:'row',
+                 alignItems:'center',
+                 justifyContent:'space-between',
+                 height:50,
+                            }}>
 
-   </View>
+   <View> <Text ><h1>Instagram</h1></Text> </View>
+    <View style={{flexDirection:'row'}}>
+      <TouchableOpacity style={{marginRight:15}}>
+        <Image  style={{height:24,width:25}} source={require('../assets/images/heart.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <View style={{position:'relative'}}>
+        <Image  style={{height:24,width:25}} source={require('../assets/images/err.png')}/>
+        <View style={{position:'absolute',bottom:12,left:13}}>
+          <Text style={{
+             backgroundColor:'red',
+             paddingHorizontal:5,
+             borderRadius:10}}>
+              5
+
+          </Text>
+        </View>
+        </View>
+      </TouchableOpacity>
+
+    </View>
+ </View>
   )
 }
 
